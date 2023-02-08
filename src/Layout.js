51ec3bfb109656526eb95bdf7e371141344c1c18
tomnyson <tabletkindfire@gmail.com>
2023-02-08 11:28:10 +0700

    add login & context

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header";
+import { ThemeContext } from "./context";
 const Layout = (props) => {
+  const location = useLocation();
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+
+  useEffect(() => {
+    setCurrentUser(JSON.parse(localStorage.getItem("user")));
+  }, [location]);
+
   return (
-    <Container>
-      <Header />
-      {props.children}
-    </Container>
+    <ThemeContext.Provider
+      value={{
+        theme: {
+          color: "green",
+        },
+      }}
+    >
+      <Container>
+        <Header auth={currentUser} />
+        {props.children}
+      </Container>
+    </ThemeContext.Provider>
   );
 };
 export default Layout;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,45 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
-const Header = () => {
+import { Nav, Button } from "react-bootstrap";
+import "./header.scss";
+import { useNavigate } from "react-router-dom";
+import { ThemeContext } from "../context";
+const Header = ({ auth }) => {
+  const navigate = useNavigate();
+  const themValue = useContext(ThemeContext);
+  console.log("themValue", themValue);
+  const renderLogout = () => {
+    return (
+      <div>
+        <Button
+          style={{ color: themValue.theme.color }}
+          variant="link"
+          onClick={logout}
+        >
+          Hi: {auth.username} (logout)
+        </Button>
+      </div>
+    );
+  };
+  const logout = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
   return (
-    <Nav activeKey="/home">
-      <Link style={{ marginRight: 5 }} to="/">
-        Home
-      </Link>
+    <Nav className="navigation" activeKey="/home">
+      <div className="header">
+        <Link style={{ marginRight: 5 }} to="/">
+          Home
+        </Link>
 
-      <Link to="/about" style={{ marginRight: 5 }}>
-        About
-      </Link>
-      <Link to="/login" style={{ marginRight: 5 }}>
-        Login
-      </Link>
+        <Link to="/about" style={{ marginRight: 5 }}>
+          About
+        </Link>
+        <Link to="/login" style={{ marginRight: 5 }}>
+          Login
+        </Link>
+      </div>
+      {auth && renderLogout()}
     </Nav>
   );
 };
